1b7031758ab1dcc59becffd8042b43f9bd65b851
aditithanekar <aditipthanekar@gmail.com>
2023-05-18 17:15:12 -0700

    added perimeter() and rectangle constructors

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -1,4 +1,29 @@
 #include "../header/rectangle.hpp"
+#include <iostream>
+using namespace std;
+
+Rectangle::Rectangle()
+{
+    width = 0; 
+    height = 0;
+}
+Rectangle::Rectangle(int w, int h)
+{
+    if(w<0)
+    {
+        throw runtime_error("Negative width");
+    }
+    width = w;
+    if(h<0)
+    {
+        throw runtime_error("Negative height");
+    }
+    height = h;
+}
+int Rectangle::perimeter()
+{
+    return -1;
+}
 
 void Rectangle::set_width(int w) {
     this->width = w;
